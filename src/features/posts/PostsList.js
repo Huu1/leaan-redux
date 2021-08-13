1930f2cf0856f71f18bcb8a1508b68153716ab18
胡瑶 <huy@qq.com>
2021-08-13 17:33:47 +0800

    数据流基础 注释

--- src/features/posts/PostsList.js
+++ src/features/posts/PostsList.js
@@ -2,7 +2,9 @@ import React from 'react'
 import { useSelector } from 'react-redux'
 
 export const PostsList = () => {
-  const posts = useSelector(state => state.posts)
+  //  useSelector选择器函数接收整个 state 对象，并且返回需要的部分数据
+  // 每当 Redux store 更新时，选择器将重新运行，如果它们返回的数据发生更改，则组件将重新渲染
+  const posts = useSelector(state => state.posts);
 
   const renderedPosts = posts.map(post => (
     <article className="post-excerpt" key={post.id}>
