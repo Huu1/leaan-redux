f84d78d66b962452bb27aef7b433f90aa840b3c9
胡瑶 <huy@qq.com>
2021-08-16 14:46:51 +0800

    使用store的数据

--- src/features/posts/AddPostForm.js
+++ src/features/posts/AddPostForm.js
@@ -1,32 +1,43 @@
 import React, { useState } from 'react'
-import { useDispatch } from 'react-redux'
-import { nanoid } from '@reduxjs/toolkit'
+import { useDispatch, useSelector } from 'react-redux'
 
 import { postAdded } from './postSlice'
 
 export const AddPostForm = () => {
   const [title, setTitle] = useState('')
   const [content, setContent] = useState('')
+  const [userId, setUserId] = useState('')
+
+
+  const users = useSelector(state => state.users)
 
   const onTitleChanged = e => setTitle(e.target.value)
   const onContentChanged = e => setContent(e.target.value)
+  const onAuthorChanged = e => setUserId(e.target.value)
 
 
   const dispatch = useDispatch()
-  
+
   const onSavePostClicked = () => {
     if (title && content) {
-      dispatch(
-        postAdded({
-          id: nanoid(),
-          title,
-          content
-        })
-      )
+
+      //  createSlice 将为我们添加到切片的每个 reducer 函数生成 action creator 函数
+      // 在组件中调用 dispatch(someActionCreator()) 来 dispatch action
+      // Reducers 将运行，检查此 action 是否相关，并在适当时返回新状态
+      dispatch(postAdded(title, content, userId))
+
       setTitle('')
       setContent('')
     }
   }
+
+  const canSave = Boolean(title) && Boolean(content) && Boolean(userId)
+
+  const usersOptions = users.map(user => (
+    <option key={user.id} value={user.id}>
+      {user.name}
+    </option>
+  ))
 
   return (
     <section>
@@ -40,6 +51,11 @@ export const AddPostForm = () => {
           value={title}
           onChange={onTitleChanged}
         />
+        <label htmlFor="postAuthor">Author:</label>
+        <select id="postAuthor" value={userId} onChange={onAuthorChanged}>
+          <option value=""></option>
+          {usersOptions}
+        </select>
         <label htmlFor="postContent">内容：</label>
         <textarea
           id="postContent"
@@ -47,7 +63,8 @@ export const AddPostForm = () => {
           value={content}
           onChange={onContentChanged}
         />
-         <button type="button" onClick={onSavePostClicked}  >保存帖子</button>
+        <button type="button" onClick={onSavePostClicked} disabled={!canSave}>
+          保存帖子</button>
       </form>
     </section>
   )
--- src/features/posts/PostsList.js
+++ src/features/posts/PostsList.js
@@ -1,17 +1,36 @@
 import React from 'react'
 import { useSelector } from 'react-redux'
+import { Link } from 'react-router-dom'
+
+import { PostAuthor } from './PostAuthor'
+import { TimeAgo } from './TimeAgo'
+import { ReactionButtons } from './ReactionButtons'
 
 export const PostsList = () => {
-  //  useSelector选择器函数接收整个 state 对象，并且返回需要的部分数据
-  // 每当 Redux store 更新时，选择器将重新运行，如果它们返回的数据发生更改，则组件将重新渲染
-  const posts = useSelector(state => state.posts);
+  const posts = useSelector((state) => state.posts)
 
-  const renderedPosts = posts.map(post => (
-    <article className="post-excerpt" key={post.id}>
-      <h3>{post.title}</h3>
-      <p className="post-content">{post.content.substring(0, 100)}</p>
-    </article>
-  ))
+  // Sort posts in reverse chronological order by datetime string
+  const orderedPosts = posts
+    .slice()
+    .sort((a, b) => b.date.localeCompare(a.date))
+
+  const renderedPosts = orderedPosts.map((post) => {
+    return (
+      <article className="post-excerpt" key={post.id}>
+        <h3>{post.title}</h3>
+        <div>
+          <PostAuthor userId={post.user} />
+          <TimeAgo timestamp={post.date} />
+        </div>
+        <p className="post-content">{post.content.substring(0, 100)}</p>
+
+        <ReactionButtons post={post} />
+        <Link to={`/posts/${post.id}`} className="button muted-button">
+          View Post
+        </Link>
+      </article>
+    )
+  })
 
   return (
     <section className="posts-list">
